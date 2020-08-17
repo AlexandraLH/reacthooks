7511c4cba7338908c6cc6847341758f3ce4c6f9d
AlexandraLH <alexandra.lhermitte@icloud.com>
2020-08-17 11:38:18 +0200

    react Hooks counter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './component/counterhooks'
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
